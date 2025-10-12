ae78108d454455abc19018e9a38b6d7786f4a2f6
Cristianrm2606 <cr204060@gmail.com>
2025-10-11 19:55:00 -0600

    Añadir SqliteDataStore service con el CRUD  de operaciones para tareas

---
+++ MiAgendaUTN-Cristian, Raul/MiAgendaUTN-Cristian, Raul/Services/IDataStore.cs
@@ -0,0 +1,15 @@
+﻿using MiAgendaUTN_Cristian__Raul.Models;
+
+namespace MiAgendaUTN_Cristian__Raul.Services
+{
+    public interface IDataStore
+    {
+        Task<int> AddTareaAsync(Tarea tarea);
+        Task<int> UpdateTareaAsync(Tarea tarea);
+        Task<int> DeleteTareaAsync(int id);
+        Task<Tarea> GetTareaAsync(int id);
+        Task<List<Tarea>> GetAllTareasAsync();
+        Task<List<Tarea>> GetTareasByCategoria(string categoria);
+        Task<List<Tarea>> GetTareasFiltradas(bool soloCompletadas = false);
+    }
+}
