8a81664f2d4a527793cc7c3392941bd8cc6b9a1d
Cristianrm2606 <cr204060@gmail.com>
2025-10-11 19:37:57 -0600

    Estructura inicial en MAUI Blazor con SQLite con paquetes de NuGet

---
+++ MiAgendaUTN-Cristian, Raul/MiAgendaUTN-Cristian, Raul/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MiAgendaUTN_Cristian__Raul
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MiAgendaUTN-Cristian, Raul/MiAgendaUTN-Cristian, Raul/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MiAgendaUTN_Cristian__Raul
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
